712fc83ab938367d14777c596f4a5a3f29045e57
Amit Sharma <shamamit11@gmail.com>
2023-03-20 23:44:04 +0400

    contact page fix

--- resources/views/site/blog/index.blade.php
+++ resources/views/site/blog/index.blade.php
@@ -16,7 +16,7 @@
     <section class="as-blog-wrapper space-top space-extra-bottom">
         <div class="container">
             <div class="row">
-                <div class="col-xxl-12 col-lg-12">
+                <div class="col-xxl-8 col-lg-7">
                     @foreach($blogs as $blog)
                         <div class="as-blog blog-single has-post-thumbnail">
                             <div class="blog-img">
@@ -35,6 +35,29 @@
                         </div>
                     @endforeach
 
+                </div>
+                <div class="col-xxl-4 col-lg-5">
+                    <aside class="sidebar-area">
+                        <div class="widget  ">
+                            <h3 class="widget_title">Recent Posts</h3>
+                            <div class="recent-post-wrap">
+                                @foreach($blogs as $blog)
+                                    <div class="recent-post">
+                                        <div class="media-img">
+                                            <a href="{{route('blog-detail', [$blog->slug])}}"><img src="{{ $blog->main_image }}" alt="{{$blog->title}}"></a>
+                                        </div>
+                                        <div class="media-body">
+                                            <h4 class="post-title" style="margin:0; font-size: 16px;"><a class="text-inherit" href="{{route('blog-detail', [$blog->slug])}}">{{$blog->title}}</a></h4>
+                                            <div class="recent-post-meta">
+                                                <a href="#"><i class="fal fa-calendar-days"></i>{{$blog->date}}</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+
+                            </div>
+                        </div>
+                    </aside>
                 </div>
             </div>
         </div>
